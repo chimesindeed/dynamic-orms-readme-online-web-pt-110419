c749803b6c30ab2d6bc67ff316e2ad5acaba98c9
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-09 17:49:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -19,6 +19,10 @@ class Song
     end
     column_names.compact
   end
+  
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
   def initialize(id = nil, name, album)
     @id = id
     @name = name
